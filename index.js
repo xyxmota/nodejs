27bd26d239c45c2f74f66e2954be815c03e3849c
lucasmota_22 <145806163+lucasmota272@users.noreply.github.com>
2025-01-27 00:07:26 -0300

    avanço no aprendizado

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 const express = require('express'); // criando uma requesição para o express
+const nodemon = require('nodemon');
 const serve = express(); // iniciando o express como servidor
-
-// http://localhost:3000/hello/:nome
-// Route params = /hello/:nome
-
-serve.get("/hello", (req, res)=> {
-    const nome = req.query.nome;
-    return res.json ({ tittle: 'Hello World!',
-        mensage: `Hello ${nome}, welcome to my first API!`
-    });
-});
-
-// http://localhost:3000/hello/?nome=valor&nome2=valor2
-// Query params = ?nome=valor&nome2=valor2
-
-serve.get("/hello", (req, res)=> {
-    const nome = req.query.nome;
-    return res.json ({ tittle: 'Hello World!',
-        mensage: `Hello ${nome}, welcome to my first API!`
-    });
-});
-
 serve.listen(3000); // iniciando o servidor na porta 3000
 
-// cara, amanha tenho que rever de novo as duas e diferenciar uma da outra, não entendi muito bem a diferença e estou com sono kkkk. entao vou dormir e amanha eu vejo isso. parei na aula 0.2.0.3 e posso rever novamente o video.
+// http://localhost:3000/hello?nome=Lucas
+// query params = ?nome=Lucas
+
+serve.get("/hello", (req, res)=> {
+    const nome = req.query.nome;
+    return res.json ({ tittle: 'Hello World!',
+        mensage: `Olá ${nome}, tudo bem com você?`
+    });
+});
+
+// http://localhost:3000/hello/Lucas
+// router params = /:nome
+serve.get("/hello/:nome", (req, res)=> {
+    const {nome}= req.params;
+    return res.json ({ tittle: 'Hello World!',
+        mensage: `Olá ${nome}, tudo bem?`
+    });
+});
+
+// vou parar aqui na aula 0.3.0.3 (utilizando o nodemon), favor lucas, seguir pra proxima aula.
